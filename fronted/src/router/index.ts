9ccd7e01c99336849656c91db6796eceb8dc108e
GHOST <3089308393@qq.com>
2025-02-28 14:33:34 +0800

    update router

--- fronted/src/router/index.ts
+++ fronted/src/router/index.ts
@@ -1,13 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SecondHandHub from '@/views/SecondHandHubView.vue'
+import Login from '@/views/LoginView.vue'
+import UserInfo from '@/views/UserInfoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
+    {
+      path: '/',
+      name: 'SecondHandHub',
+      component: SecondHandHub,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/UserInfo',
+      name: 'UserInfo',
+      component: UserInfo
+    }
   ],
 })
 
